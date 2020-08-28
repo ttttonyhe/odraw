05c45e68bb0ffd7f1a01d7bf1eb40c70014ca95a
HelipengTony <tony.hlp@hotmail.com>
2020-08-28 13:13:58 +0800

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,11 @@
 // 页面顶部
+import { GithubOutlined } from "@ant-design/icons";
 const Footer = () => {
   return (
     <div className="odraw-footer">
-      <p>&copy; 2020 安徽工程大学团委</p>
+      <p>
+        &copy; 2020 安徽工程大学团委&nbsp;·&nbsp;<a href="https://github.com/HelipengTony/odraw" target="_blank"><GithubOutlined /></a>
+      </p>
     </div>
   );
 };
